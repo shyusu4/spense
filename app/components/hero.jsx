6e092aaa1e4ff7d301be5c39438841a8f26c9ade
shyusu4 <shyusupova4@gmail.com>
2023-02-12 18:24:24 +0500

    Create components folder

---
+++ app/components/hero.jsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+  return (
+    <>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 py-8 top-5 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+              Share your unfiltered thoughts. Get paid.
+            </h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+              Spense is an open platform for individuals to share their
+              unfiltered thoughts. Discuss the topics you love, and get paid for
+              doing just that.
+            </p>
+          </div>
+          <div class="hidden lg:mt-0 lg:col-span-5 lg:flex"></div>
+        </div>
+      </section>
+    </>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Navbar from './navbar'
-
+import Image from 'next/image';
+import styles from './page.module.css';
+import Navbar from './components/navbar';
+import Hero from './components/hero';
 export default function Home() {
   return (
-    <Navbar />
-  )
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
+
